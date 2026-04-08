e0df381e07025600e9e5fcb3f03bb511132e57db
Mads A. Pedersen <mads.amtoft@hotmail.com>
2026-04-08 17:13:42 +0200

    readded rtl files

--- verilog/rtl/firrtl_black_box_resource_files.f
+++
@@ -1,4 +0,0 @@
-/Users/mads/Desktop/projects/dtu-soc-2026/verilog/rtl/CaravelUserProject.ScratchPadMem.MEM.v
-/Users/mads/Desktop/projects/dtu-soc-2026/verilog/rtl/CaravelUserProject.ScratchPadMem.MEM_1.v
-/Users/mads/Desktop/projects/dtu-soc-2026/verilog/rtl/CaravelUserProject.ScratchPadMem.MEM_2.v
-/Users/mads/Desktop/projects/dtu-soc-2026/verilog/rtl/CaravelUserProject.ScratchPadMem.MEM_3.v
