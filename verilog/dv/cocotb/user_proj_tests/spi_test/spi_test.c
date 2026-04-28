fefd73419adff4dc60128211ce5662a2819ba64c
prote <proteopro@gmail.com>
2026-04-28 16:05:03 +0200

    [fix] Fix test using wrong RAM bank after comm_controller addition

--- verilog/dv/cocotb/user_proj_tests/spi_test/spi_test.c
+++ verilog/dv/cocotb/user_proj_tests/spi_test/spi_test.c
@@ -1,38 +1,53 @@
 #include <firmware_apis.h>
 
-#define IMEM_BASE (0x0200000 >> 2)
+#define IMEM_2_ADDR (0x300000 >> 2) // imem bank 1
+#define COMM_CTRL   (0x400000 >> 2)
 
-void main(void) {
+void main() {
     ManagmentGpio_outputEnable();
     ManagmentGpio_write(0);
     enableHkSpi(0);
 
-    User_enableIF();
-
-    // Write Wildcat's program into WishboneInstrRam
-    // objdump of apps/spi_psram_a.s:
-    //   0x0: 0x50000137  lui  sp, 0x50000  (sp = 0x5000_0000)
-    //   0x4: 0x00012183  lw   gp, 0(sp)    (read PSRAM A CS1_n LOW)
-    //   0x8: 0xFFDFF06F  j    0x4          (loop forever)
-    USER_writeWord(0x50000137, IMEM_BASE + 0);
-    USER_writeWord(0x00012183, IMEM_BASE + 1);
-    USER_writeWord(0xFFDFF06F, IMEM_BASE + 2);
-
-    // Unlock Wildcat
-    // From WishboneInstrRam.v: cpuEnable sets when io_wb_addr[9:2] == 8'hff
-    // That means Wishbone byte address offset = 0xFF << 2 = 0x3FC
-    // As a word index: IMEM_BASE + (0x3FC >> 2) = IMEM_BASE + 0xFF
-    USER_writeWord(0x1, IMEM_BASE + 0xFF);
-
-    // Group 5 SPI pins
-    GPIOs_configure(0, GPIO_MODE_USER_STD_OUTPUT);
-    GPIOs_configure(1, GPIO_MODE_USER_STD_OUTPUT);
-    GPIOs_configure(2, GPIO_MODE_USER_STD_INPUT_NOPULL);
-    GPIOs_configure(3, GPIO_MODE_USER_STD_OUTPUT);
-    GPIOs_configure(4, GPIO_MODE_USER_STD_OUTPUT);
-    GPIOs_configure(5, GPIO_MODE_USER_STD_OUTPUT);
+    // Configure Group 5 SPI Pins
+    GPIOs_configure(0, GPIO_MODE_USER_STD_OUTPUT);       // CS0_n
+    GPIOs_configure(1, GPIO_MODE_USER_STD_OUTPUT);       // MOSI
+    GPIOs_configure(2, GPIO_MODE_USER_STD_INPUT_NOPULL); // MISO
+    GPIOs_configure(3, GPIO_MODE_USER_STD_OUTPUT);       // SCK
+    GPIOs_configure(4, GPIO_MODE_USER_STD_OUTPUT);       // CS1_n
+    GPIOs_configure(5, GPIO_MODE_USER_STD_OUTPUT);       // CS2_n
 
     GPIOs_loadConfigs();
+    User_enableIF();
 
-    ManagmentGpio_write(1);
+    // RESET (Bit 0 = 1) AND select IMEM_2 (Bit 1 = 1)
+    // Binary 11 = 3
+    USER_writeWord(3, COMM_CTRL);
+
+    uint32_t boot_program[] = {
+        0x50000137, // lui x2, 0x50000  (Loads 0x50000000)
+        0x00012183, // lw x3, 0(x2)     (read PSRAM A CS1_n LOW)
+        0xFFDFF06F  // j loop           (loop forever)
+    };
+
+    for (int i = 0; i < 3; i++) {
+        USER_writeWord(boot_program[i], IMEM_2_ADDR + i);
+    }
+
+    // verify saved program
+    bool status = true;
+    for (int i = 0; i < 3; i++) {
+        if (USER_readWord(IMEM_2_ADDR + i) != boot_program[i]) {
+            status = false;
+        }
+    }
+
+    if (status == true) {
+        // Release RESET (Bit 0 = 0) BUT KEEP IMEM_2
+        // Binary 10 = 2
+        USER_writeWord(2, COMM_CTRL);
+
+        ManagmentGpio_write(1);
+    }
+
+    return;
 }
